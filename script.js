f42d6ee3cc0e0f022517145226f0efc7d58b63d8
Shiva Dhakal <theskdhakal@gmail.com>
2023-01-08 23:51:13 +1100

    some more

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
 function findPerimeter(length, width) {
   return 2 * (length + width);
 }
+
+// You are counting points for a basketball game, given the amount of 2-pointers scored and 3-pointers scored, find the final points for the team and return that value.
+
+function points(twoPointers, threePointers) {
+  let a = 2 * twoPointers;
+  let b = 3 * threePointers;
+
+  return a + b;
+}
+
+// Write a function that returns the string "something" joined with a space " " and the given argument a.
+function giveMeSomething(a) {
+  return "something" + " " + a;
+}
